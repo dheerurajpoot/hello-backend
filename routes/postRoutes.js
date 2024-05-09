fb56934548eccbb37a1333adf8d1e21b50a32e15
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2024-05-09 21:28:49 +0530

    add get all posts for everyone

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,12 +1,18 @@
 import express from "express";
-import { creatPost, deletePost, getAllPosts, likeDislike } from "../controllers/postControler.js";
+import {
+	creatPost,
+	deletePost,
+	getAllPosts,
+	getPosts,
+	likeDislike,
+} from "../controllers/postControler.js";
 import userAuth from "../middilewares/userAuth.js";
 
 const router = express.Router();
-router.route('/create').post(userAuth , creatPost);
-router.route("/delete/:id").delete(userAuth ,deletePost);
+router.route("/create").post(userAuth, creatPost);
+router.route("/delete/:id").delete(userAuth, deletePost);
 router.route("/like/:id").put(userAuth, likeDislike);
 router.route("/allposts/:id").get(userAuth, getAllPosts);
-
+router.route("/posts/").get(getPosts);
 
 export default router;

42711e12f092720a6621a65ecdd78d52424cfca8
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2024-05-09 21:41:18 +0530

    update frontend cors

--- index.js
+++ index.js
@@ -18,9 +18,13 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+// cors
 app.use(cors());
 app.use(function (req, res, next) {
-	res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+	res.setHeader(
+		"Access-Control-Allow-Origin",
+		"https://hellosocial.vercel.app"
+	);
 	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
 	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 	res.setHeader("Access-Control-Allow-Credentials", true);

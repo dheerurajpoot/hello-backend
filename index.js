70b234012f7ddf280ad5d41daaa007b581162a02
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2024-05-09 21:10:11 +0530

    add cors

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
-import express from 'express';
-import cookieParser from 'cookie-parser'
-import dbConnect from './config/dbconnect.js';
-import 'dotenv/config';
-import userRoute from './routes/userRoutes.js';
-import postRoute from './routes/postRoutes.js';
-import cors from 'cors'
+import express from "express";
+import cookieParser from "cookie-parser";
+import dbConnect from "./config/dbconnect.js";
+import "dotenv/config";
+import userRoute from "./routes/userRoutes.js";
+import postRoute from "./routes/postRoutes.js";
+import cors from "cors";
 
-dbConnect()
+dbConnect();
 const app = express();
 
 //middilewares
-app.use(express.urlencoded({
-  extended:true
-}));
+app.use(
+	express.urlencoded({
+		extended: true,
+	})
+);
 app.use(express.json());
 app.use(cookieParser());
 
-const corsOption ={
-  // origin:"http://localhost:5173",
-  origin:"https://hello-frontend-livid.vercel.app",
-  credentials:true
-}
-app.use(cors(corsOption));
+const corsOpts = {
+	origin: "*",
 
-app.get('/', function (req, res) {
-  res.send('Server is running.....')
+	methods: ["GET", "POST"],
+
+	allowedHeaders: ["Content-Type"],
+};
+
+app.use(cors(corsOpts));
+
+app.get("/", function (req, res) {
+	res.send("Server is running.....");
 });
 
-//api or url
-app.use("/api/v1/user", userRoute)
-app.use("/api/v1/post", postRoute)
+//api urls
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/post", postRoute);
 
-app.listen(process.env.PORT, ()=>{
-  console.log(`Server is listening on port ${process.env.PORT}`);
-})
+app.listen(process.env.PORT, () => {
+	console.log(`Server is listening on port ${process.env.PORT}`);
+});

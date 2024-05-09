dd51e0886d70c8b3fbe1e4ec8e1fedd825353e53
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2024-05-09 21:15:01 +0530

    update cors

--- index.js
+++ index.js
@@ -18,16 +18,14 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-const corsOpts = {
-	origin: "*",
-
-	methods: ["GET", "POST"],
-
-	allowedHeaders: ["Content-Type"],
-};
-
-app.use(cors(corsOpts));
-
+app.use(cors());
+app.use(function (req, res, next) {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+	res.setHeader("Access-Control-Allow-Credentials", true);
+	next();
+});
 app.get("/", function (req, res) {
 	res.send("Server is running.....");
 });
